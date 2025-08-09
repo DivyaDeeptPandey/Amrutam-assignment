985189b81138508a50d4eeb90ac4e626f0e6ecf5
DivyaDeeptPandey <divyadeeptpandey@gmail.com>
2025-08-09 20:55:10 +0530

    final commit

--- server/config/db.js
+++ server/config/db.js
@@ -4,11 +4,7 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-    const connection = mongoose.connection;
-    connection.once('open', () => {
-      console.log(`Connected to DB: ${connection.name}`);
-    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`, " ",conn.connection.name);
   } catch (error) {
     console.error('MongoDB Connection Error:', error.message);
     process.exit(1);
